d81e8f10208928a7026bd3f01f8130fad7820591
Yilun Zhou <yz192@duke.edu>
2016-09-22 13:52:01 -0400

    Add cell class API

--- src/cellsociety_team06/Cell.java
+++ src/cellsociety_team06/Cell.java
@@ -1,5 +1,27 @@
 package cellsociety_team06;
 
+import java.util.*;
+
 public class Cell {
 
+	private ArrayList<Cell> neighbors;
+	private CellValue value;
+	private CellValue nextValue;
+	private Rule rule;
+	
+	/**
+	 * Prepare for update. Change nextValue based on neighbors.
+	 * Needs to consider rule. 
+	 */
+	public void prepareForUpdate() {
+		
+	};
+	
+	/**
+	 * update. Replace value with nextValue.
+	 */
+	public void update() {
+		value = nextValue;
+	}
+	
 }
--- src/cellsociety_team06/SpecificationFileParser.java
+++ src/cellsociety_team06/SpecificationFileParser.java
@@ -1,5 +1,17 @@
 package cellsociety_team06;
+import java.util.*;
 
 public class SpecificationFileParser {
 
+	private Rule rule;
+	private HashMap<Cell, ArrayList<Cell>> board; // adjacency list representation of board
+	
+	/**
+	 * Read XML file. Fill rule and board. 
+	 * @param name filename
+	 */
+	public void readFile(String name) {
+		
+	}
+	
 }
